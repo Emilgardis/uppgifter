e7bfb255e3744266b1fa57e47e72bee404b4e991
Emil Gardström <emil.gardstrom@gmail.com>
2014-12-22 22:56:24 +0100

    fixed documentation

--- pr1/uppgift2/uppgift21.py
+++ pr1/uppgift2/uppgift21.py
@@ -1,22 +1,31 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Hitta största palindrom tal med produkt av två 3-siffriga tal
 
 
 def fastpal(number):
+    """Returns True if number is a palindrome.
+
+    A palindrome is read the same forwards as backwards.
+    matches the original.
+    Keyword arguments:
+    number -- number to be palindrome checked
+
+    This is achieved with a simple slice reversal of the string equivalent of
+    given number.
+    """
+
     return str(number) == str(number)[::-1]
 
 
 def main():
-    n = [-1]
+    n = [-1]  # contains highest yet multiple
     for i in range(1, 999):
         for k in range(1, 999):
-            j = i * k
-            # if i > 950 and k > 900 and k < 950:
-            # print "%d * %d = %d" % (i, k, j)
-            if fastpal(j) and j > n[0]:
-                # print "%d is a palindrome" % j
+            j = i * k  # Makes a temporary variable for palindrome checking.
+            if fastpal(j) and j > n[0]:  # If palindrome and > than previous.
                 n = [j, i, k]
-    print "%d (%d * %d) is the highest possible palindrome" % (n[0], n[1], n[2])
+    print "{} ({} * {}) is the highest possible palindrome consisting of 3 de"\
+        "cimal places".format(n[0], n[1], n[2])
     return n[0]
 
 if __name__ == '__main__':
